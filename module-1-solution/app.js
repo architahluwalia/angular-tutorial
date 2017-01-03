c3bc0717c27ca6f2febf3baa872a44fc018572cc
archit_ahluwalia <archit.ahluwalia@gmail.com>
2017-01-03 22:21:30 +0530

    deploy ready

---
+++ module-1-solution/app.js
@@ -0,0 +1,27 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+	$scope.showMsg =false;
+	$scope.menu = '';
+	$scope.checkIfTooMuch = function () {
+		$scope.showMsg = true;
+		let menuArray = $scope.menu.split(',').filter(function(v){return v!==''});
+		if (menuArray.length == 0) {
+			$scope.color = 'red';			
+			$scope.msg = 'Please Enter data!';
+		} else if (arr.length>3) {
+			$scope.color = 'green';			
+			$scope.msg = 'Too Much!';
+		} else {
+			$scope.color = 'green';			
+			$scope.msg = 'Enjoy!';
+		}			
+	};
+}
+
+})();
